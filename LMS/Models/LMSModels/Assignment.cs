f0da7400ef85341d6f63425d3ecbb75c5977758f
anshul kasera <anshulkasera73@gmail.com>
2025-04-01 18:28:23 -0600

    Initial commit

---
+++ LMS/Models/LMSModels/Assignment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LMS.Models.LMSModels
+{
+    public partial class Assignment
+    {
+        public Assignment()
+        {
+            Submissions = new HashSet<Submission>();
+        }
+
+        public string Name { get; set; } = null!;
+        public uint Points { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Contents { get; set; } = null!;
+        public uint CategoryId { get; set; }
+        public uint AssignmentId { get; set; }
+
+        public virtual AssignmentCategory Category { get; set; } = null!;
+        public virtual ICollection<Submission> Submissions { get; set; }
+    }
+}
---
+++ LMS/Models/LMSModels/Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LMS.Models.LMSModels
+{
+    public partial class Class
+    {
+        public Class()
+        {
+            AssignmentCategories = new HashSet<AssignmentCategory>();
+            Enrolleds = new HashSet<Enrolled>();
+        }
+
+        public string Location { get; set; } = null!;
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+        public ushort SemYear { get; set; }
+        public string SemSeason { get; set; } = null!;
+        public uint CourseId { get; set; }
+        public string UId { get; set; } = null!;
+        public uint ClassId { get; set; }
+
+        public virtual Course Course { get; set; } = null!;
+        public virtual Professor UIdNavigation { get; set; } = null!;
+        public virtual ICollection<AssignmentCategory> AssignmentCategories { get; set; }
+        public virtual ICollection<Enrolled> Enrolleds { get; set; }
+    }
+}
